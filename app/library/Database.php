362ddd372f1d61a0b9f05f0fb87b2630a6920629
Passerby <jpshen@vip.qq.com>
2018-01-05 11:59:48 +0800

    增加数据库批量查询方法

--- app/library/Database.php
+++ app/library/Database.php
@@ -5,35 +5,35 @@
  * @package        Hooloo Framework
  * @author         Passerby, Bill
  * @version        1.2
- * @release        2017.10.27
+ * @release        2018.1.5
  */
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class Database
 {
-    private $db_handle;
-    private $db_result;
+    private $_mysqli;
+    private $_result;
     
     // 连接数据库
     public function connect($address, $username, $password, $db_name, $charset = 'utf8')
     {
-        $this->db_handle = new mysqli($address, $username, $password, $db_name);
-        $this->db_handle->set_charset($charset);
+        $this->_mysqli = new mysqli($address, $username, $password, $db_name);
+        $this->_mysqli->set_charset($charset);
     }
     
     // 自定义SQL查询语句
     public function query($sql)
     {
-        $this->db_result = $this->db_handle->query($sql);
+        $this->_result = $this->_mysqli->query($sql);
         return $this;
     }
     
     // 返回单条数据
     public function row_array()
     {
-        if (false !== $this->db_result) {
-            $res = $this->db_result->fetch_assoc();
-            $this->db_result->free();
+        if (false !== $this->_result) {
+            $res = $this->_result->fetch_assoc();
+            $this->_result->free();
         } else {
             $res = array();
         }
@@ -44,11 +44,11 @@ class Database
     public function result_array()
     {
         $res = array();
-        if (false !== $this->db_result) {
-            while($row = $this->db_result->fetch_assoc()) {
+        if (false !== $this->_result) {
+            while ($row = $this->_result->fetch_assoc()) {
                 $res[] = $row;
             }
-            $this->db_result->free();
+            $this->_result->free();
         }
         return $res;
     }
@@ -56,46 +56,66 @@ class Database
     // 返回最后插入id
     public function insert_id()
     {
-        return $this->db_handle->insert_id;
+        return $this->_mysqli->insert_id;
     }
     
     // 返回影响函数
     public function affected_rows() {
-        return $this->db_handle->affected_rows;
+        return $this->_mysqli->affected_rows;
+    }
+    
+    // 批量查询语句
+    public function multi_query($sql)
+    {
+        $this->_result = $this->_mysqli->multi_query($sql);
+        return $this;
+    }
+    
+    // 返回多个结果集
+    public function results()
+    {
+        $res = array();
+        $i = 0;
+        if (false !== $this->_result) {
+            do {
+                if ($result = $this->_mysqli->store_result()) {
+                    while ($row = $result->fetch_assoc()) {
+                        $res[$i][] = $row;
+                    }
+                    $result->free();
+                }
+                $i++;
+            } while ($this->_mysqli->more_results() && $this->_mysqli->next_result());
+        }
+        return $res;
     }
     
     // 开启事务
     public function trans_begin()
     {
         // 设置为不自动提交，因为MYSQL默认立即执行
-        mysqli_query($this->db_handle, 'SET AUTOCOMMIT = 0');
+        $this->_mysqli->autocommit(false);
         // 开始事务定义
-        mysqli_begin_transaction($this->db_handle);
+        $this->_mysqli->begin_transaction();
     }
     
     // 回滚事务
     public function trans_rollback()
     {
         // 判断当执行失败时回滚
-        mysqli_query($this->db_handle, 'ROLLBACK');
+        $this->_mysqli->rollback();
     }
     
     // 提交事务
     public function trans_commit()
     {
         // 执行事务
-        mysqli_commit($this->db_handle);
+        $this->_mysqli->commit();
     }
     
     // 关闭数据库连接
     public function close()
     {
-        $this->db_handle->close();
-    }
-    
-    // 返回MySql版本
-    public function getserver_info()
-    {
-        return mysqli_get_server_info($this->db_handle);
+        $this->_mysqli->close();
     }
 }
